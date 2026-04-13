22d7aec5979c0a21c1691a9a0337d19b647101d0
Tyler Pires <93809267+Tylercpires@users.noreply.github.com>
2026-04-13 13:59:20 -0700

    Birdle Updates
    - game.dart file added
    - Tile widget created
    - GamePage widget created
    - Main scaffolding for game created

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 //Created 2026-04-12
 //Last Updated 2026-04-12
 
+import 'game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,11 +14,77 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!!'),
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
         ),
+        body: Center(child: GamePage()),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  //Constructor
+  final String letter;
+  final HitType hitType;
+  //
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch(hitType){
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for(var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for(var letter in guess)
+                  Tile(letter.char, letter.type),
+              ],
+            ),
+        ],
       ),
     );
   }
